acc8d45e8a84e1fb78194c050db3d28ec7aa9587
cjproud <christian.proud@curtin.edu.au>
2023-06-10 10:41:32 +0800

    Reset and just change url patterns.

--- azure_auth/urls.py
+++ azure_auth/urls.py
@@ -4,7 +4,7 @@ from azure_auth.views import azure_auth_callback, azure_auth_login, azure_auth_l
 
 app_name = "azure_auth"
 urlpatterns = [
-    path("login", azure_auth_login, name="login"),
-    path("logout", azure_auth_logout, name="logout"),
-    path("callback", azure_auth_callback, name="callback"),
+    path("sign_in", azure_auth_login, name="login"),
+    path("sign_out", azure_auth_logout, name="logout"),
+    path("redirect", azure_auth_callback, name="callback"),
 ]
